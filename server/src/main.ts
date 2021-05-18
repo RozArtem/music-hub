da7ac3e173691a78db11853a8da26f0c63e1b320
RozArtem <rozum13art@gmail.com>
2021-05-18 16:51:17 +0300

    add  validation pipe

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,6 +6,11 @@ async function
 start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
+
+
+  app.useGlobalPipes(new ValidationPipe())
+
+
   await app.listen(PORT, () => {
 
     console.log(`server has been running on port ${PORT}`)

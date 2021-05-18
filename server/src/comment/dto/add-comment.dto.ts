010576e57bae8bada7a2c7ed3d56cd07fc30b1f0
RozArtem <rozum13art@gmail.com>
2021-05-18 16:53:26 +0300

    add valitation for all dto

--- server/src/comment/dto/add-comment.dto.ts
+++ server/src/comment/dto/add-comment.dto.ts
@@ -1,4 +1,7 @@
+import { IsString } from "class-validator";
+
 export class AddCommentDTO {
 
+    @IsString({message: 'have to be string'})
     readonly description: string;
 }
--- server/src/track/dto/creat-track.dto.ts
+++ server/src/track/dto/creat-track.dto.ts
@@ -1,5 +1,8 @@
+import { IsString } from "class-validator";
+
 export class CreatTackDTO {
 
+    @IsString({message: 'have to be string'})
     readonly name: string;
     
 }

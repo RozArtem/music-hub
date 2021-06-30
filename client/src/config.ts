4589dc31a4783ea9ed8708baceb4b975fd3a6032
RozArtem <rozum13art@gmail.com>
2021-06-30 18:56:27 +0300

    change

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'https://www.musichubsocial.ml/'
+export const API_URL = 'http://www.musichubsocial.ml:8080/'
 export const STATIC_URL = 'http://www.musichubsocial.ml:8080/'
 
 //'http://18.119.99.223:5000/'

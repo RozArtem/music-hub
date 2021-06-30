968c9b0512cfca713af283c6c8c3578afe4709c2
RozArtem <rozum13art@gmail.com>
2021-06-30 18:46:01 +0300

    fix for static url

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 export const API_URL = 'https://www.musichubsocial.ml/'
-export const STATIC_URL = 'http://www.musichubsocial.ml:8080'
+export const STATIC_URL = 'http://www.musichubsocial.ml:8080/'
 
 //'http://18.119.99.223:5000/'
 

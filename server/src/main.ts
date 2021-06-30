15797fad484a269c3366c1f81cea5a77d38c3c96
RozArtem <rozum13art@gmail.com>
2021-06-30 20:37:33 +0300

    https server options

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs'
 
 async function 
 start() {
+
+
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/musichubsocial.ml/fullchain.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/musichubsocial.ml/privkey.pem'),
+  };
+
   const PORT = process.env.PORT || 5000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions
+  });
 
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe())

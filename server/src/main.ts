1dd9ef3eda22f1b1d902554446e7cfdcd58816d7
RozArtem <rozum13art@gmail.com>
2021-06-30 21:08:12 +0300

    fix setting

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,14 +3,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs'
 
+
+
+const httpsOptions = {
+  key: fs.readFileSync('./etc/letsencrypt/live/musichubsocial.ml/fullchain.pem'),
+  cert: fs.readFileSync('./etc/letsencrypt/live/musichubsocial.ml/privkey.pem'),
+};
+
 async function 
 start() {
 
 
-  const httpsOptions = {
-    key: fs.readFileSync('/etc/letsencrypt/live/musichubsocial.ml/fullchain.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/musichubsocial.ml/privkey.pem'),
-  };
 
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule, {

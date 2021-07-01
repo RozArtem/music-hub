86bd04b6c2e7dc025355fadbbce72a0a06bf5e23
RozArtem <rozum13art@gmail.com>
2021-07-01 15:59:51 +0300

    new selected strack page

--- client/src/pages/track/TrackPage.tsx
+++ client/src/pages/track/TrackPage.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import { useHistory } from 'react-router'
 import { API_URL } from '../../config'
@@ -7,65 +7,90 @@ import { useTypedSelector } from '../../hooks/useTypeSelector'
 import './trackpage.css'
 import Faav from '../../components/trakc/fav/Faav'
 import AlbumList from '../../components/album/AlbumList'
+import { useActions } from '../../hooks/useActions'
 
 const TrackPage: React.FC = () => {
+
+
+    const { DeleteTrackFromCurrentProfile, getOneTrack} = useActions()
 
     const { currentTrack } = useTypedSelector(state => state.track)
     const { albums } = useTypedSelector(state => state.album)
     const { currentProfile } = useTypedSelector(state => state.users)
-    const { isAuth } = useTypedSelector(state => state.currentUser)
+    const { isAuth, currentUser } = useTypedSelector(state => state.currentUser)
     const [showAlbumsBlock, setShowAlbumsBlock] = useState<boolean>(false)
+    let [owner, setOwner] = useState<boolean>(false)
+
     const history = useHistory()
+    const location = history.location.pathname.split('/');
+
+
+    function deleteTrack(e: any) {
+
+        e.stopPropagation()
+        currentTrack && DeleteTrackFromCurrentProfile(currentTrack?.id)
+
+    }
+
+
+    useEffect(() => {
+
+
+        if (currentUser?.id === currentTrack?.authorID) { setOwner(true) }
+        getOneTrack(location[2])
+
+    }, [])
 
     return (
         <div className='track-page' >
             <Navbar />
-            <div className='track-page___container'>
-
-                <div className="func">
-                    < button className="track-page___container_btn"
+            <div className="track-page___container">
+                <div className="track-page___container_func">
+                    < button className="track-page___container_func_btn"
                         onClick={() => history.goBack()}
-                    >BACK</button>
-                    {isAuth &&
+                    >
+                        BACK
+                    </button>
+                    <div className="track-page___container_func_pic">
+                        <img className="track-page___container_func_pic_img"
+                            src={API_URL + currentTrack?.picture} alt="trakc img" />
 
-                        <div className="func-container">
-                            <Faav track={currentTrack} />
-                            {albums.length > 1 &&
+                    </div>
+                    <div className="track-page___container_func_func">
+                        <Faav track={currentTrack} />
+                        {albums.length > 1 &&
 
-                                <div className='item___func___add-to-album'
+                            <div className='track-page___container_func_func_add-to-album'
 
-                                    onMouseEnter={() => { setShowAlbumsBlock(true) }}
-                                    onMouseOver={() => { setShowAlbumsBlock(true) }}
-                                    onMouseLeave={() => { setShowAlbumsBlock(false) }}
-                                >{!showAlbumsBlock && '+'}</div>
-                            }
-                        </div>
+                                onMouseEnter={() => { setShowAlbumsBlock(true) }}
+                                onMouseOver={() => { setShowAlbumsBlock(true) }}
+                                onMouseLeave={() => { setShowAlbumsBlock(false) }}
+                            >{!showAlbumsBlock && '+'}</div>
+                        }
+                        {owner && <div className='track-page___container_func_func_delete'
+                            onClick={(e) => { deleteTrack(e) }}
+                        >х</div>}
+                        {isAuth && showAlbumsBlock &&
 
+                            <AlbumList setShowAlbumsBlock={setShowAlbumsBlock} track={currentTrack} albums={albums} />
+                        }
+                    </div>
 
-
-                    }
-                    {isAuth && showAlbumsBlock &&
-
-                        <AlbumList setShowAlbumsBlock={setShowAlbumsBlock} track={currentTrack} albums={albums} />
-                    }
                 </div>
 
-
-                <div className="track-page___container_info">
-
-                    <img src={API_URL + currentTrack?.picture} alt="picOfTrack" />
-                    <div className="track-page___container_info_data">
-                        <p className='track-page___container_info_name'>
-                            {currentTrack?.name}
-                        </p>
-                        <div className="track-page___container_info_author"
-
-                            onClick={() => { history.push(`/profile/${currentProfile?.id}`) }}
-
-                        >
-                            uploaded by: {currentProfile?.name}
-                        </div>
+                <div className="track-page___container__info">
+                    <div className="track-page___container_info_name">
+                        {currentTrack?.name}
                     </div>
+
+                    <div className="track-page___container_info_author"
+
+                        onClick={() => { history.push(`/profile/${currentProfile?.id}`) }}
+
+                    >
+                        uploaded by: {currentProfile?.name}
+                    </div>
+
                 </div>
 
             </div>
